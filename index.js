c16d13db50d7549a04c69a6a22742ba8806e84ae
omnagare <omnagare07@gmail.com>
2025-03-30 17:52:52 +0530

    This is Continous Devlivery

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const PORT = 8080
+const path = require('path')
+
+app.set('view engine' , 'ejs')
+app.set("views" , path.join(__dirname , "views"))
+
+
+app.get('/' , (req , res)=>{
+    res.render('main')
+})
+
+app.listen(PORT , ()=> console.log(`SERVER IS RUNNING ON PORT ${PORT}`))
